3d97866d57d582b0170ddb965f63f5c046758a3d
ViaticusRex <vdelacruz417@gmail.com>
2024-08-06 11:02:44 -0400

    finished holy grail layout and made it responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,47 @@
-import { useState } from 'react'
-import './App.css'
+import {Routes, Route} from 'react-router-dom'
 
-function App() {
+import NavBar from './components/NavBar'
+import Home from './pages/Home'
+import Services from './pages/Services'
+import ShowService from './pages/ShowService'
+import AddService from './pages/AddService'
+import EditService from './pages/EditService'
+
+
+
+import './styles/App.css'
+
+//Holy Grail Layout
+const App = () => {
+    return (
+      <div className="app_container">
+        <header>
+          <NavBar />
+        </header>
+        
+        <aside className='sidebar_left'>
+          // left sidebar content TBD
+        </aside>
   
-
-  return (
-   
-  )
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/services/:id" element={<ShowService />} />
+            <Route path="/services/add" element={<AddService />} />
+            <Route path="/services/:id/edit" element={<EditService />} />
+          </Routes>
+        </main>
+  
+        <aside className='sidebar_right'>
+            // right sidebar content TBD
+        </aside>
+  
+        <footer className='footer'>
+            // footer
+        </footer>
+      </div>
+    )
 }
 
 export default App
